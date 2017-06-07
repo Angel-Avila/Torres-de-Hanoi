e13ee17a7d6f7abfb853313eb75c2e32daba891d
Angel Avila <Angel@Angels-MacBook-Pro.local>
2017-06-06 19:46:57 -0500

    Initial commit

---
+++ hannoi.asm
@@ -0,0 +1,11 @@
+# Ángel Armando Ávila Chavira is697755
+# Cristhian Franco Reynoso    is697163
+
+.text
+Main:
+	addi $s0, $zero, 3	# Número de discos
+				# s1 torre de inicio
+				# s2 torre temporal
+				# s3 torre destino
+
+Exit:
